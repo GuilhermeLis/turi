82a9888002a58b07e0763c5bb921aa3d9498fa0f
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-10-20 18:17:20 -0300

    Update auth.service.ts
    retirando comentários

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -35,38 +35,7 @@ export class AuthService {
     return firebase.auth().currentUser;
   }
 
-  googleLogin(){
-
-  //  var provider = new firebase.auth.GoogleAuthProvider()
-  //  provider.addScope('profile');
-  //  provider.addScope('email');
-  //  firebase.auth().signInWithRedirect(provider);
-
-  //  firebase.auth().signInWithPopup(provider).then(function(result) {
-  //  This gives you a Google Access Token.
-  //  var token = result.credential
-  //    The signed-in user info.
-  //    var user = result.user;
-  //  });
-    /*.googleLogin()
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      //var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-    */
-  }
+  googleLogin(){}
 
   loginWithEmail(user: Login) {
     return new Promise((resolve, reject) => {
